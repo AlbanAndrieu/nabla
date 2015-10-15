78ab7cf78870ba96118fd40fb20adf9bbc51b918
Andrieu, Alban <alban.andrieu@free.fr>
2015-10-15 04:17:51 -0500

    Change groovy runtime and add circular enforcer

---
+++ env/linux/run-strace.sh
@@ -0,0 +1,3 @@
+#http://www.cyberciti.biz/faq/howto-use-linux-truss-strace-command/
+strace -o /tmp/output.txt tail -f /workspace/users/albandri30/nabla-servers/sample/jsf-simple/deploy.log
+#use truss on solaris
