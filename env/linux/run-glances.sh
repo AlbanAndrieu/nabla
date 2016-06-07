94fff3612c4a11b319b10701890fdc0ee1e38524
AlbanAndrieu <alban.andrieu@free.fr>
2016-06-07 17:06:59 -0500

    Cleaning

---
+++ env/linux/run-glances.sh
@@ -0,0 +1,4 @@
+#https://github.com/nicolargo/glances
+pip install --upgrade pip
+pip install glances
+pip install bottle batinfo https://bitbucket.org/gleb_zhulik/py3sensors/get/tip.tar.gz zeroconf netifaces pymdstat influxdb potsdb statsd pystache docker-py pysnmp pika py-cpuinfo scandir
