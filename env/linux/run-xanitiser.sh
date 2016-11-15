15d2171787a919b8cb661a5fd59f786021ba5d0c
Andrieu, Alban <alban.andrieu@free.fr>
2016-11-15 11:46:08 +0100

    Split security

---
+++ env/linux/run-xanitiser.sh
@@ -0,0 +1,5 @@
+
+#See https://www.rigs-it.net/index.php/product/sonarqube-integration.html
+
+cd /workspace/divers
+wget https://www.rigs-it.net/opendownloads/wsanojs-Xanitizer-2.4/Xanitizer-2.4.6-lin64-obf.zip
---
+++ env/scripts/jenkins/nabla-security-monitor-chrootkit-admin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo ""
+echo "################### CHROOTKIT ###################"
+echo ""
+
+sudo chkrootkit -V
+
+sudo chkrootkit -q
+
+#sudo tiger --version
+
+#sudo tiger
