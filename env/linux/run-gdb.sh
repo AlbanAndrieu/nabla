18a46d6e551a2207b28407a872f97efb27f05481
Andrieu, Alban <alban.andrieu@free.fr>
2017-01-23 17:57:47 +0100

    Update

---
+++ env/linux/run-gdb.sh
@@ -0,0 +1,8 @@
+#See https://fahdshariff.blogspot.fr/2012/08/analysing-java-core-dump.html
+cat /proc/sys/kernel/core_pattern
+
+gdb $JAVA_HOME/bin/java core.11051
+jstack -J-d64 $JAVA_HOME/bin/java core.11051
+jmap -J-d64 $JAVA_HOME/bin/java core.11051
+
+#Eclipse MAT memory analyzer or use jvisualvm
