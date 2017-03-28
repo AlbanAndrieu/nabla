11643e8743527cb3a09a8006d3182574ca1e13e0
AlbanAndrieu <alban.andrieu@free.fr>
2017-03-28 12:27:09 +0200

    Fixes

--- env/scripts/jenkins/nabla-security-monitor-docker-admin.sh
+++ env/scripts/jenkins/nabla-security-monitor-docker-admin.sh
@@ -4,21 +4,21 @@ set -xv
 echo ########### Check Docker disk space usage #######
 
 #Thre reference
-sudo -t docker history hello-world:latest
+docker history hello-world:latest
 
-sudo -t docker ps -a -s
+docker ps -a -s
 
 echo ########### Check Docker images tree #######
 
 #See https://github.com/justone/dockviz
 alias dockviz="sudo docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz"
-sudo -t docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz images -t
-sudo -t docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz containers -d | dot -Tpng -o containers.png
-sudo -t docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz images --dot --only-labelled | dot -Tpng -o images.png
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz images -t
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz containers -d | dot -Tpng -o containers.png
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz images --dot --only-labelled | dot -Tpng -o images.png
 
 echo ########### Check Docker security #######
 
-sudo -t docker run -it --net host --pid host --cap-add audit_control \
+docker run -it --net host --pid host --cap-add audit_control \
     -v /var/lib:/var/lib \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /usr/lib/systemd:/usr/lib/systemd \
