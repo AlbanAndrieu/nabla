92ef63dd568afe9e90c973c67b1ab7acd369e36e
Andrieu, Alban <alban.andrieu@misys.com>
2017-05-19 15:03:23 +0200

    Upgrade node/npm version

---
+++ env/linux/run-scons.sh
@@ -0,0 +1,13 @@
+#https://github.com/icebreaker/scolorizer
+
+#See https://bitbucket.org/scons/scons/wiki/SconsToolbox
+#See https://bitbucket.org/scons/scons/wiki/SconsRecipes
+
+#Add https://bitbucket.org/scons/scons/wiki/BashCompletion
+
+#See https://github.com/ja11sop/cuppa
+
+sudo apt install ubuntu-dev-tools
+reverse-depends -b scons
+
+#See http://www.cs.virginia.edu/~dww4s/tools/scons2dot/
