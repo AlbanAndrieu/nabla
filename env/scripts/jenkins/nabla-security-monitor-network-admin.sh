248dfbe41e3f456c7c9795faba43627b540fe2af
Andrieu, Alban <alban.andrieu@misys.com>
2017-10-26 21:34:12 +0200

    Apache SSL

--- env/scripts/jenkins/nabla-security-monitor-network-admin.sh
+++ env/scripts/jenkins/nabla-security-monitor-network-admin.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #set -xve
 
-./step-2-0-0-build-env.sh
+source ./step-2-0-0-build-env.sh
+
+echo "https://www.ssllabs.com/ssltest/analyze.html?d=nabla.freeboxos.fr&s=82.253.244.162"
 
 echo -e "${NC}"
 echo -e "${magenta} ${underline}###################${NC}"
