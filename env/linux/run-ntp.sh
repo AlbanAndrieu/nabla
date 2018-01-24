382928dd103e6b9f7d0982b9375d3ba4a5edae9c
Andrieu, Alban <alban.andrieu@misys.com>
2018-01-24 10:15:51 +0100

    Update

--- env/linux/run-ntp.sh
+++ env/linux/run-ntp.sh
@@ -9,3 +9,15 @@ ntpdate timehost
 hwclock --systohc
 hwclock --show
 /etc/init.d/ntpd start
+
+#On RHEL7
+#systemctl list-unit-files | grep ntpd
+systemclt status ntpd.service
+systemctl stop chronyd
+#systemctl enable dhcpd.service
+#systemctl enable named.service
+systemctl enable ntpd.service
+#systemctl enable httpd.service
+systemctl start ntpd.service
+
+exit 0
--- env/linux/run-python.sh
+++ env/linux/run-python.sh
@@ -3,8 +3,8 @@ set -xv
 
 #disable rhnplugin
 # grep enable /etc/yum/pluginconf.d/rhnplugin.conf
-enabled = 0
-#enabled = 1
+#enabled = 0
+##enabled = 1
 
 #or disable by hand
 yum search python26 --disableplugin=rhnplugin
@@ -15,7 +15,7 @@ tar xvfz Python-2.7.3.tgz # unzip
 cd Python-2.7.3 # go into directory
 ./configure
 make # build
-su # or 'sudo su' if there is no root user
+#su # or 'sudo su' if there is no root user
 make altinstall
 #make install #do not do it
 
@@ -26,3 +26,37 @@ ls -lrta /usr/local/bin/python2.7
 rm /root/Python-2.7.3
 
 #might need todo yum remove mercurial
+
+#TODO RedHat https://www.tecmint.com/install-python-in-linux/ install python 3.6 for ansible
+#yum -y groupinstall development
+#yum -y install zlib-devel
+
+# wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tar.xz
+# tar xJf Python-3.6.3.tar.xz
+# cd Python-3.6.3
+# ./configure
+# make
+# make install
+
+# which python3
+# python3 -V
+
+#virtualenv py3-ansible -p /usr/local/bin/python3.6
+
+#alternatives --list | grep -i python
+
+#alternatives --install /usr/bin/python python /usr/local/bin/python3.6 2
+#alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+
+#pip3 install docker-py
+#pip3 install -U docker-compose
+#pip3 install ansible
+
+#python3 /usr/bin/ansible localhost -m ping
+
+#update-alternatives --config python
+
+#Show outdated pip package
+pip list --outdated --format=freeze
+#Upgrade all
+pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo pip install -U
