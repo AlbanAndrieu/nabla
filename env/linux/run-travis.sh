5b73d06067b4f78f94e231221734abae30b43bda
Andrieu, Alban <alban.andrieu@misys.com>
2018-02-09 22:21:51 +0100

    sonar

--- env/linux/run-travis.sh
+++ env/linux/run-travis.sh
@@ -3,4 +3,11 @@
 
 #See https://docs.travis-ci.com/user/environment-variables/
 
+sudo gem install travis
+/usr/local/bin/travis encrypt SOMEVAR="secretvalue" --add
+
+travis pubkey
+
+echo "https://api.travis-ci.org/repos/AlbanAndrieu/nabla-cpp/key"
+
 exit 0
