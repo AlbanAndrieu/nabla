2932e08db6e26cf7a1715451663bc9b64c1c89ab
Andrieu, Alban <alban.andrieu@misys.com>
2018-11-15 00:31:46 +0100

    Update terraform

---
+++ env/linux/run-lamp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#set -xv
+
+sudo apt-get install tasksel
+sudo tasksel
+
+exit 0
--- env/linux/run-terraform.sh
+++ env/linux/run-terraform.sh
@@ -7,7 +7,8 @@ cd nabla-servers-bower-sample
 terraform init
 
 #Echo organisation : Banbou - https://github.com/Banbou
-terraform apply
+#terraform apply
+terraform apply -var 'key_name=terraform' -var 'public_key_path=~/.ssh/id_rsa.pub' -var 'github_organization=Banbou' -var "github_token=${GITHUB_TOKEN}"
 
 terraform show
 
