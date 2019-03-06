3fdef03997f49adb13e58583762515df329b632a
AlbanAndrieu <alban.andrieu@free.fr>
2019-03-06 09:14:42 +0100

    Move pass out

--- env/linux/run-test-kitchen.sh
+++ env/linux/run-test-kitchen.sh
@@ -4,7 +4,7 @@ set -xv
 # See https://kitchen.ci/
 
 sudo apt-get install ruby gem -y
- 
+
 sudo gem install test-kitchen kitchen-vagrant kitchen-ansible kitchen-docker
 
 #kitchen init --driver=vagrant --provisioner=ansible
