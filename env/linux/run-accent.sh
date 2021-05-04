7d975f17098d95e50d49d52b47f5db88ef099110
Andrieu, Alban <alban.andrieu@free.fr>
2021-05-04 19:16:51 +0200

    latext texstudio

--- env/linux/run-accent.sh
+++ env/linux/run-accent.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 echo "keycode 115 = Multi_key" | xmodmap -
 
-#https://forum.ubuntu-fr.org/viewtopic.php?id=6125
-#Pour info, il peut être utile de vérifier si la touche de composition n'est pas désactivée. C'était le cas lors de mon installation d'Ubuntu 16.04.
-#Pour cela, aller dans Clavier --> Saisie --> Touche de composition, et changer le Désactivé par autre chose (j'ai choisi Ctrl gauche).
+# See https://askubuntu.com/questions/358/how-can-i-type-accented-characters-like-%C3%AB
+
+# https://forum.ubuntu-fr.org/viewtopic.php?id=6125
+# Pour info, il peut être utile de vérifier si la touche de composition n'est pas désactivée. C'était le cas lors de mon installation d'Ubuntu 16.04.
+# Pour cela, aller dans Clavier --> Saisie --> Touche de composition, et changer le Désactivé par autre chose (j'ai choisi Ctrl gauche).
 ##### Ubuntu 16.04 LTS
 # Typing -> Compose Key -> Right Win
 
--- env/linux/run-latex.sh
+++ env/linux/run-latex.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #set -xv
 
-sudo apt-get install latexila
-
 sudo apt-get install texlive-full
 
 sudo apt-get install texlive texlive-latex-extra texlive-lang-french
@@ -10,6 +8,12 @@ sudo apt-get install texlive texlive-latex-extra texlive-lang-french
 sudo apt-get install texmaker
 
 texmaker cv-aandrieu-02-07-2020.tex
-pdflatex cv-aandrieu-02-07-2020.tex
+
+pdflatex -synctex=1 -interaction=nonstopmode %.tex
+#pdflatex cv-aandrieu-02-07-2020.tex
+
+# See https://doc.ubuntu-fr.org/texstudio
+sudo apt-get install texstudio
+#sudo apt-get install latexila
 
 exit 0
